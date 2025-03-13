7b3dc1f8bfe880dc6c8eb378aa91fcbc7547bc02
Maximiliano Valdivia <Valdiviamaximiliano20@gmail.com>
2025-03-13 00:12:48 -0300

    Se mejoró la estética con un diseño más atractivo en CSS y se implementó la redirección funcional desde el botón jugar, se editan codigo de algunos archivos

---
+++ public/js/acciones.js
@@ -0,0 +1,6 @@
+// Asignar evento al botón "Jugar"
+document.getElementById('btn-jugar').addEventListener('click', () => {
+    // Redirigir al usuario a la página del juego
+    window.location.href = 'juego.html';
+  });
+  

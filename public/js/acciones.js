8f5d925dc772fdc787b74a8c007ad2e77cf220db
Maximiliano Valdivia <Valdiviamaximiliano20@gmail.com>
2025-03-13 21:37:13 -0300

    Se eliminan carpetas y archivos innecesarios que solo estorban en la we o que no tienen un uso, se deja lo necesario para el correcto funcionamiento

--- public/js/acciones.js
+++
@@ -1,6 +0,0 @@
-// Asignar evento al botón "Jugar"
-document.getElementById('btn-jugar').addEventListener('click', () => {
-    // Redirigir al usuario a la página del juego
-    window.location.href = 'juego.html';
-  });
-  
